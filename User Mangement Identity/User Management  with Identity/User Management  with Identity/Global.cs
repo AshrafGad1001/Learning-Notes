2b65d885fed96ab37187cd9c0198c894add2753e
AshrafGad1001 <ashrafgad542@gmail.com>
2024-05-04 23:34:10 +0300

    ASP

--- User Mangement Identity/User Management  with Identity/User Management  with Identity/Global.cs
+++
@@ -1,4 +0,0 @@
-﻿global using Microsoft.AspNetCore.Identity;
-global using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
-global using Microsoft.EntityFrameworkCore;
-global using User_Management__with_Identity.Models;
--- User Mangement Identity/User Management  with Identity/User Management  with Identity/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace User_Management__with_Identity.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
